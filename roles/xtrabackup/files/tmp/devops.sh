9c08254d5d7f17ba8376cf4f8f7960cd91059808
LAPTOP-TGV01QNG\lenovo <eddie222.diamoand@gmail.com>
2020-09-04 20:03:11 +0800

    f

--- roles/xtrabackup/files/tmp/devops.sh
+++ roles/xtrabackup/files/tmp/devops.sh
@@ -4,7 +4,7 @@
 ## xtrabackup 8.0 版本 适应 MySQL Server 8.0 与 Percona Server 8.0
 
 ######################################## xtrabackup 2.4 版本 ########################################
-sudo innobackupex --user=backup --passwd=mysql_pass_backup --socket=/var/lib/mysql/mysql.sock --defaults- file=/etc/my.cnf /tmp/xtrabackup/full --no-timestamp
+sudo innobackupex --use-memory=4G --deaults-file=/etc/my.cnf --host=127.0.0.1 --user=root --password=mysql_pass_cli /var/www/xtrabackup
 
 
 ######################################## xtrabackup 8.0 版本 ########################################
@@ -17,7 +17,7 @@ sudo xtrabackup --backup --host=localhost --user=backup --password=mysql_pass_ba
 ## 回复需 
 ## sudo rm -R /var/lib/mysql/*
 ## sudo chown -R mysql:mysql /var/lib/mysql
-sudo xtrabackup --defaults-file=/etc/my.cnf --copy-back --rsync --target-dir=/home/vagrant/www/xtrabackup
+sudo xtrabackup --defaults-file=/etc/my.cnf --copy-back --rsync --target-dir=/var/www/xtrabackup/2020-09-04_19-30-08
 
 sudo xtrabackup --prepare --host=localhost --user=backup --password=mysql_pass_backup  --target-dir=./xtrabackup --parallel=2
 
@@ -30,28 +30,6 @@ sudo xtrabackup --prepare --apply-log-only  --use-memory=4G --user=backup --pass
 ########################################## 备份 并还原 单一表的方法 ########################################## 
 
 sudo xtrabackup --defaults-file=/etc/my.cnf --copy-back --rsync --target-dir=/home/vagrant/www/xtrabackup
-
-mysql -e "DROP TABLE landan.users;"
-mysql -e "USE landan;
-          CREATE TABLE `users` (
-            `user_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-            `name` VARCHAR(45) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci' NOT NULL,
-            `password` CHAR(96) CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci' NULL, # md5(users.name . KEY). md5(md5(users.password) . strrev(md5(users.password . KEY)))
-            `expired_time` DATETIME NOT NULL DEFAULT '2100-01-01 00:00:00',
-            `created_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-            `updated_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
-            `deleted_time` DATETIME NULL DEFAULT NULL,
-            PRIMARY KEY (`user_id`))
-          -- ENGINE = InnoDB
-          DEFAULT CHARACTER SET = utf8mb4 PARTITION BY LINEAR HASH(user_id) PARTITIONS 8 ;";
-          
-mysql -e "ALTER TABLE landan.users DISCARD TABLESPACE;"
-
-sudo cp /home/vagrant/www/xtrabackup/landan/users*.ibd /var/lib/mysql/landan
-
-sudo chown -R mysql:mysql /var/lib/mysql/landan
-
-mysql -e "ALTER TABLE landan.users IMPORT TABLESPACE;"
 
 ########################################## 备份 并还原 单一表的方法 ########################################## 
 
