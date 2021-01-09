ec55ad9a18a1f6bf2131b4d2a0bb99ad559aa784
doroke7 <doroke7@hotmail.com>
2021-01-10 02:50:00 +0800

    FF

--- roles/supervisor/files/tmp/devops.sh
+++ roles/supervisor/files/tmp/devops.sh
@@ -22,31 +22,31 @@ sudo supervisorctl reload;
 sudo supervisorctl shutdown;
 
 # 进入 supervisord CLI 控制界面
-sudo supervisorctl -c /etc/supervisord.conf
+sudo supervisorctl -c /etc/supervisord/supervisord.conf
 
 # 进入 supervisord CLI 控制界面
-sudo supervisorctl -c /etc/supervisord.conf status
+sudo supervisorctl -c /etc/supervisord/supervisord.conf status
 
 # 进入 supervisord CLI 控制界面  读取有更新（增加）的配置文件，不会启动新添加的程序
-sudo supervisorctl -c /etc/supervisord.conf reread
+sudo supervisorctl -c /etc/supervisord/supervisord.conf reread
 
 # 进入 supervisord CLI 控制界面  重启配置文件修改过的程序
-sudo supervisorctl -c /etc/supervisord.conf update
+sudo supervisorctl -c /etc/supervisord/supervisord.conf update
 
 
 # 重启 lottery-service_resource_backend-lumen GROUP 所有 命令 #  【要加 冒号!!!!!】【要加 冒号!!!!!】【要加 冒号!!!!!】【要加 冒号!!!!!】 
-sudo supervisorctl -c /etc/supervisord.conf restart lottery-service_resource_backend-lumen:
+sudo supervisorctl -c /etc/supervisord/supervisord.conf restart lottery-service_resource_backend-lumen:
 
 # 重启 lottery-crawler_all_backend-sracpy GROUP 所有 命令 【要加 冒号!!!!!】【要加 冒号!!!!!】【要加 冒号!!!!!】【要加 冒号!!!!!】 
-sudo supervisorctl -c /etc/supervisord.conf restart lottery-crawler_all_backend-sracpy:
+sudo supervisorctl -c /etc/supervisord/supervisord.conf restart lottery-crawler_all_backend-sracpy:
 
 
 
 # 关闭 lottery-service_resource_backend-lumen php73_artisan_swoole_http_start
-sudo supervisorctl -c /etc/supervisord.conf stop lottery-service_resource_backend-lumen:php73_artisan_swoole_http_start
+sudo supervisorctl -c /etc/supervisord/supervisord.conf stop lottery-service_resource_backend-lumen:php73_artisan_swoole_http_start
 
 # 启动 lottery-service_resource_backend-lumen php73_artisan_swoole_http_start
-sudo supervisorctl -c /etc/supervisord.conf start lottery-service_resource_backend-lumen:php73_artisan_swoole_http_start
+sudo supervisorctl -c /etc/supervisord/supervisord.conf start lottery-service_resource_backend-lumen:php73_artisan_swoole_http_start
 
 
 # 注意 supervisorctl (Supervisorctl CLI) 前提是要 supervisord 开启
